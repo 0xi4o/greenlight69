7aa027e2bd086e3ee58384e654781476b8427abd
Ilango <rajagopalilango@gmail.com>
2024-08-23 11:12:06 +0530

    Setup connection to a postgres database

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,20 +1,31 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"flag"
 	"fmt"
+	"github.com/joho/godotenv"
 	"net/http"
 	"os"
 	"time"
 
 	"github.com/charmbracelet/log"
+
+	_ "github.com/lib/pq"
 )
 
 const version = "1.0.0"
 
 type config struct {
-	port int
+	db struct {
+		dsn          string
+		maxOpenConns int
+		maxIdleConns int
+		maxIdleTime  time.Duration
+	}
 	env  string
+	port int
 }
 
 type application struct {
@@ -25,11 +36,38 @@ type application struct {
 func main() {
 	var cfg config
 
+	logger := log.NewWithOptions(os.Stdout, log.Options{ReportCaller: true, ReportTimestamp: true})
+
+	err := godotenv.Load()
+	if err != nil {
+		logger.Error("error reading .env", "err", err.Error())
+		os.Exit(1)
+	}
+
+	var (
+		dbName   = os.Getenv("DB_NAME")
+		username = os.Getenv("DB_USERNAME")
+		password = os.Getenv("DB_PASSWORD")
+		host     = os.Getenv("DB_HOST")
+	)
+
+	cfg.db.dsn = fmt.Sprintf("postgres://%s:%s@%s/%s?sslmode=disable", username, password, host, dbName)
+
 	flag.IntVar(&cfg.port, "port", 4000, "API Server Port")
 	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
+	flag.IntVar(&cfg.db.maxOpenConns, "db-max-open-conns", 25, "PostgreSQL maximum open connections")
+	flag.IntVar(&cfg.db.maxIdleConns, "db-max-idle-conns", 25, "PostgreSQL maximum idle connections")
+	flag.DurationVar(&cfg.db.maxIdleTime, "db-max-idle-time", 15*time.Minute, "PostgreSQL maximum connection idle time")
 	flag.Parse()
 
-	logger := log.NewWithOptions(os.Stdout, log.Options{ReportCaller: true, ReportTimestamp: true})
+	db, err := openDB(cfg)
+	if err != nil {
+		logger.Error("error opening db connection", "err", err.Error())
+		os.Exit(1)
+	}
+	defer db.Close()
+
+	logger.Info("database connection pool established")
 
 	app := &application{
 		config: cfg,
@@ -47,7 +85,29 @@ func main() {
 
 	logger.Info("starting server", "addr", cfg.port, "env", cfg.env)
 
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	logger.Error(err.Error())
 	os.Exit(1)
+}
+
+func openDB(cfg config) (*sql.DB, error) {
+	db, err := sql.Open("postgres", cfg.db.dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	db.SetMaxOpenConns(cfg.db.maxOpenConns)
+	db.SetMaxIdleConns(cfg.db.maxIdleConns)
+	db.SetConnMaxIdleTime(cfg.db.maxIdleTime)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err = db.PingContext(ctx)
+	if err != nil {
+		db.Close()
+		return nil, err
+	}
+
+	return db, nil
 }
--- go.mod
+++ go.mod
@@ -3,11 +3,16 @@ module greenlight.i4o.dev
 go 1.22.5
 
 require (
+	github.com/charmbracelet/log v0.4.0
+	github.com/julienschmidt/httprouter v1.3.0
+)
+
+require (
 	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
 	github.com/charmbracelet/lipgloss v0.10.0 // indirect
-	github.com/charmbracelet/log v0.4.0 // indirect
 	github.com/go-logfmt/logfmt v0.6.0 // indirect
-	github.com/julienschmidt/httprouter v1.3.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/lib/pq v1.10.9 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/mattn/go-isatty v0.0.18 // indirect
 	github.com/mattn/go-runewidth v0.0.15 // indirect
