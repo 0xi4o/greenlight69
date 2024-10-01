9e686bb1a08f2b722b1ce1f376a5a17949e1abb9
Ilango <rajagopalilango@gmail.com>
2024-10-01 07:39:56 +0530

    Complete chapter 18 - metrics

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,16 +3,19 @@ package main
 import (
 	"context"
 	"database/sql"
+	"expvar"
 	"flag"
 	"fmt"
-	"github.com/joho/godotenv"
-	"greenlight.i4o.dev/internal/data"
-	"greenlight.i4o.dev/internal/mailer"
 	"os"
+	"runtime"
 	"strconv"
 	"strings"
 	"sync"
 	"time"
+
+	"github.com/joho/godotenv"
+	"greenlight.i4o.dev/internal/data"
+	"greenlight.i4o.dev/internal/mailer"
 
 	"github.com/charmbracelet/log"
 
@@ -108,6 +111,20 @@ func main() {
 	defer db.Close()
 
 	logger.Info("database connection pool established")
+
+	expvar.NewString("version").Set(version)
+
+	expvar.Publish("goroutines", expvar.Func(func() any {
+		return runtime.NumGoroutine()
+	}))
+
+	expvar.Publish("database", expvar.Func(func() any {
+		return db.Stats()
+	}))
+
+	expvar.Publish("timestamp", expvar.Func(func() any {
+		return time.Now().Unix()
+	}))
 
 	app := &application{
 		config: cfg,
